3d16fd34d3d232a6c66a7578d427070fecff1354
forhad35 <forhadj35@gmail.com>
2025-06-16 17:22:51 +0600

    Add Windows build workflow

--- android/app/src/main/kotlin/com/example/mill_info/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mill_info
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/mealapp/devrefat/mealapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mealapp.devrefat.mealapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
